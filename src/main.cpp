ee546beceb998a36afb6f2e366fd97f403f4e8f8
addisgithub <addis.bogale@ucollege.edu>
2021-02-01 09:14:37 -0600

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 /**
  * @file main.cpp
- * @brief This is a test of CMake, doxygen, and GitHub.
- * @details This is the long brief at the top of main.cpp.
- * @author Seth McNeill
- * @date 1/28/2021
+ * @brief This is a addis testing CMake, doxygen, and GitHub.
+ * @details This documentation method will be used to pubish & document projects straight form VScode.
+ * @author Addis Bogale
+ * @date 1/31/2021
  * 
  */
 
